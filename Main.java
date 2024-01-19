a3701f6605198408d6aea46422e4f9af9406ed5a
DiegoRaC14 <141955933+DiegoRaC14@users.noreply.github.com>
2024-01-18 23:45:41 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+package clases_y_objetos;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Cuadrado miCuadrado = new Cuadrado("cuadrado", 177195);
+		Circulo miCirculo = new Circulo("círculo", 177195);
+		
+		miCuadrado.lado /= 1000;
+		miCirculo.radio /= 1000;
+		
+		
+		System.out.println("El " + miCuadrado.nombreFigura + " tiene perímetro: " + miCuadrado.calcularPerimetro(miCuadrado.lado) + ".");
+		System.out.println("El " + miCuadrado.nombreFigura + " tiene área: " + miCuadrado.calcularArea(miCuadrado.lado) + ".");
+		System.out.println("El " + miCuadrado.nombreFigura + " tiene diagonal: " + miCuadrado.calcularDiagonal(miCuadrado.lado) + ".");
+		System.out.println("");
+		System.out.println("El " + miCirculo.nombreFigura + " tiene perímetro: " + miCirculo.calcularPerimetro(miCirculo.radio) + ".");
+		System.out.println("El " + miCirculo.nombreFigura + " tiene área: " + miCirculo.calcularArea(miCirculo.radio) + ".");
+		
+	}
+
+}
