78685b596316c9ffbd8f30a10eb823a18ca97f94
DiegoRaC14 <141955933+DiegoRaC14@users.noreply.github.com>
2024-01-25 23:54:15 -0600

    Add files via upload

---
+++ Actividad3/Circulo.java
@@ -0,0 +1,42 @@
+package actividad.opciones;
+
+public class Circulo {
+	
+	public static final double PI = 3.14159; //Declaración de la constante PI
+	
+	
+	//Atributos
+	public double radio;
+	public String nombreFigura;
+	
+	//Métodos constructor
+	public Circulo (String nombreFigura, double radio) {
+		
+		this.nombreFigura = nombreFigura;
+		this.radio = radio;
+	}
+	
+	 public Circulo(String nombreFigura) {//Asigna el valor 177195 en el radio si no se especifica en main
+	        
+		 this(nombreFigura, 177195);
+	    }
+
+	
+	//Métodos
+	public double calcularPerimetro(double radio){
+		
+		 double perimetro;
+		 perimetro = PI * (2 * radio);
+		 
+		 return perimetro;
+	}
+
+	public double calcularArea(double radio){
+		
+		double area;
+		area = Math.PI * (radio*radio);
+		
+		return area;
+	}
+	
+}
