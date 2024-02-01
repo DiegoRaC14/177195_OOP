bbb58d4df32e21264db150fa049072c5757dfa8e
DiegoRaC14 <141955933+DiegoRaC14@users.noreply.github.com>
2024-02-01 00:43:22 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package examen.parcial1;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Ejecucion miPrograma = new Ejecucion();//Creación del objeto Ejecucion para llamar un método
+		
+		miPrograma.iniciarPrograma();//Llamar el método
+	}
+
+}
