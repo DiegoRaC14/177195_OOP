643bafc860ae486fce2c9b4241d65c4065ab26cc
DiegoRaC14 <141955933+DiegoRaC14@users.noreply.github.com>
2024-04-07 10:04:26 -0600

    Add files via upload

---
+++ ExamenParcial3/Main.java
@@ -0,0 +1,12 @@
+package resolver.laberinto;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Ejecutar miPrograma = new Ejecutar ();
+		miPrograma.iniciarPrograma();
+		
+		
+	}
+
+}
---
+++ ExamenParcial3/Raton.java
@@ -0,0 +1,5 @@
+package resolver.laberinto;
+
+public class Raton {
+
+}
