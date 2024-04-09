ac072c8186573f74b4e9fe410a35475df174afcd
DiegoRaC14 <141955933+DiegoRaC14@users.noreply.github.com>
2024-04-09 15:20:14 -0600

    Add files via upload

--- ExamenParcial2/Ejecutar.java
+++ ExamenParcial2/Ejecutar.java
@@ -1,79 +1,44 @@
-package guess.who;
+package examen3;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Ejecutar {
 	
-	public void iniciarPrograma() {
+	char [][] laberinto;
 	
-		Menu miMenu = new Menu ();
-		Aleatorio miAleatorio = new Aleatorio(); // Objeto clase aleatorio para llamar métodos
+	public void iniciarPrograma () {
 		
-		String ANSI_RESET = "\033[0m" ;
-		String ANSI_BLACKB = "\033[1;30m"; //Black bold
-		String ANSI_CYANB = "\033[1;36m"; // Cyan Bold
-		String ANSI_PURPLEBR= "\033[0;95m"; // Purple bright
-		String ANSI_YELLOW = "\033[0;33m";
-		String ANSI_WHITEBACK = "\033[47m";  // White background
+		Scanner sc = new Scanner(System.in); // Objeto tipo Scanner
+		Archivo miArchivo = new Archivo();
 		
-		String[] arrAtaques = {"Hydro pump", "Solar beam", "Eruption", "Flamethrower",
-						"Aqua Jet", "Whirpool", "Synthesis", "Petal Dance"};
+		String nombreArchivo;
+        
+        try {
+	         System.out.println("Ingrese el nombre del archivo que contiene el laberinto.");
+	         System.out.print("(Debe estar en la misma carpeta que el programa y debe ser .txt): ");
+	         nombreArchivo = sc.nextLine();
+	         Laberinto miLaberinto = new Laberinto(nombreArchivo + ".txt");
+	         
+	         laberinto = miLaberinto.leerLaberinto(); // Método para leer el archivo
+	         miLaberinto.imprimirLaberinto(); // Método para imprimir el laberinto en consola
+	         
+	         
+	         Raton raton = new Raton(laberinto); //Crea un ratón y manda el laberinto obtenido de archivo .txt
+	         ArrayList<String> ruta = raton.encontrarRuta(); //Guarda la lista
+	         //System.out.println("Movimientos:" + ruta);
+	         miArchivo.grabarArchivo(ruta, nombreArchivo);
+	            
+	         sc.close(); // Cerrar el Scanner
+        } catch (IOException e) {
+        	 e.printStackTrace();
+        }
+        
+        System.out.println();
+        System.out.println("El laberinto fue resuelto con éxito.");
+        System.out.println("Puede ver el archivo .txt en la carpeta donde se encuentra el programa.");
 		
-		
-		Pokemon[] arrPokemones = new Pokemon[15]; // Creación arreglo de objetos
-		
-		arrPokemones[0] = new Agua ("Squirtle", miAleatorio.asignarAtaques(arrAtaques));
-		arrPokemones[1] = new Agua ("Totodile", miAleatorio.asignarAtaques(arrAtaques));
-		arrPokemones[2] = new Agua ("Mudkip", miAleatorio.asignarAtaques(arrAtaques));
-		arrPokemones[3] = new Agua ("Froakie",miAleatorio.asignarAtaques(arrAtaques));
-		arrPokemones[4] = new Agua ("Quaxly",miAleatorio.asignarAtaques(arrAtaques));
-		
-		arrPokemones[5] = new Fuego ("Charmander",miAleatorio.asignarAtaques(arrAtaques));
-		arrPokemones[6] = new Fuego ("Cyndaquil", miAleatorio.asignarAtaques(arrAtaques));
-		arrPokemones[7] = new Fuego ("Torchic", miAleatorio.asignarAtaques(arrAtaques));
-		arrPokemones[8] = new Fuego ("Litten", miAleatorio.asignarAtaques(arrAtaques));
-		arrPokemones[9] = new Fuego ("Fuecoco",miAleatorio.asignarAtaques(arrAtaques));
-		
-		arrPokemones[10] = new Planta ("Bulbasaur", miAleatorio.asignarAtaques(arrAtaques));
-		arrPokemones[11] = new Planta ("Chikorita", miAleatorio.asignarAtaques(arrAtaques));
-		arrPokemones[12] = new Planta ("Treecko", miAleatorio.asignarAtaques(arrAtaques));
-		arrPokemones[13] = new Planta ("Rowlet", miAleatorio.asignarAtaques(arrAtaques));
-		arrPokemones[14] = new Planta ("Sprigatito", miAleatorio.asignarAtaques(arrAtaques));
-		
-		int indexPGanador;
-		Pokemon []arrPokemonesRand = new Pokemon [arrPokemones.length]; // Creación 
-		Pokemon pokemonGanador;
-		
-		System.out.println("¡Bienvenid@ a tu juego favorito...");
-		System.out.println(ANSI_YELLOW + "...ADIVINA QUIÉN DE POKEMONES!" + ANSI_RESET);
-		System.out.println();
-		System.out.println();
-		
-		System.out.println("A continuación se muestra tu tablero:");
-		System.out.println();
-		
-		arrPokemonesRand = miAleatorio.desplegarMenuRand(arrPokemones); // Guardar arreglo de pokemones aleatorio e imprimir la lista
-		indexPGanador = miAleatorio.pokemonAdvinar(arrPokemones); // Almacena el índice del Pokemon elegido al azar
-		pokemonGanador = arrPokemonesRand[indexPGanador]; // Copia info del Pokemkon elegido al azar
-		
-		//System.out.println(pokemonGanador.getNombre());
-		System.out.println();
-		System.out.println(ANSI_PURPLEBR + "Nota: Recuerda que tienes máximo 4 intentos y al 5to debes advinar el nombre" + ANSI_RESET);
-
-		System.out.println();
-		System.out.print(ANSI_CYANB + "Intento 1:" + ANSI_RESET);
-		miMenu.mostrarPreguntas(arrPokemonesRand, indexPGanador, arrAtaques, pokemonGanador, 1);
-
-		for (int i = 2; i <= 5; i++) {
-		    System.out.println();
-		    System.out.print(ANSI_CYANB + "Intento " + i + ":" + ANSI_RESET);
-		    int intento = i;
-		    miMenu.mostrarPreguntas(miMenu.nuevoArregloP, indexPGanador, arrAtaques, pokemonGanador, intento);
-		}
-
-		System.out.println();
-		System.out.println();
-		System.out.println(ANSI_BLACKB + ANSI_WHITEBACK + "NOOOOOOO...  ¡PERDISTE! :c" + ANSI_RESET);
-		System.out.println("El Pokemon correcto era:" + pokemonGanador.getNombre() + pokemonGanador.getTipo());		
 	}
 
-	
 }
